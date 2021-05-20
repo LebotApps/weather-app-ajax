5623d8d758f84e391167362d9b0d7e214db2fa2c
Lebot Apps <lebot.llc@gmail.com>
2021-05-20 16:07:31 -0400

    lab update

---
+++ js/script.js
@@ -0,0 +1,12 @@
+API CODE afdbbad862024298ca4f6eada7e55c50
+
+let weatherAPI = $.ajax({
+    url:`https://www.api.openweathermap.org/data/2.5/forecast?q={city name},{state code},{country code}&appid={afdbbad862024298ca4f6eada7e55c50}`
+    })
+
+// let citySearch = $.ajax({
+//     url:`https://www.api.openweathermap.org/data/2.5/forecast?id={city ID}&appid={afdbbad862024298ca4f6eada7e55c50}`
+// })
+
+
+
