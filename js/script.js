e6c9451404737993f12efa4f94eccd12fc4ee145
Lebot Apps <lebot.llc@gmail.com>
2021-05-21 09:52:00 -0400

    finalupload

--- js/script.js
+++ js/script.js
@@ -1,12 +1,52 @@
-API CODE afdbbad862024298ca4f6eada7e55c50
-
-let weatherAPI = $.ajax({
-    url:`https://www.api.openweathermap.org/data/2.5/forecast?q={city name},{state code},{country code}&appid={afdbbad862024298ca4f6eada7e55c50}`
-    })
-
-// let citySearch = $.ajax({
-//     url:`https://www.api.openweathermap.org/data/2.5/forecast?id={city ID}&appid={afdbbad862024298ca4f6eada7e55c50}`
-// })
+// API CODE afdbbad862024298ca4f6eada7e55c50
 
 
+let returnedData;
 
+let $inputText;
+const $returnedLocation = $('#returnedLocation');
+const $returnedWeather = $('#returnedWeather');
+const $returnedFeelsLike = $("#returnedFeelsLike");
+const $returnedTemperature = $('#returnedTemperature');
+
+
+
+
+function handleGetData(event) {
+    event.preventDefault();
+
+    inputText = $("#searchForm").val();
+    //alert(inputText);
+
+$.ajax({
+
+
+    url: `https://api.openweathermap.org/data/2.5/weather?q=${inputText}&appid=9e2e79e449de63b165683ac1fcb83225&units=imperial`,
+})
+
+
+
+
+
+.then(
+    function (data) {
+      returnedData = data;
+
+      $returnedLocation.text(returnedData["name"]);
+      $returnedFeelsLike.text(returnedData["main"]["feels_like"]);
+      //alert(returnedData["main"]["feels_like"]);
+
+    $returnedTemperature.text(data["main"]["temp"]);
+
+    $returnedWeather.text(returnedData["weather"][0]["description"]);
+
+    
+    
+    },
+    function (error) {
+      console.log("bad request: ", error);
+    }
+  );
+}
+
+$("form").on('submit', handleGetData);
